50b85d3cffbfee003e445c9a8832533744774625
smyk <k.smykala@gmail.com>
2014-10-18 11:17:41 +0200

    SignalR has been added, basic shoutbox (to all) has been added. #2

---
+++ LiveChat/LiveChatApp/Controllers/ChatController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace LiveChatApp.Controllers
+{
+    public class ChatController : Controller
+    {
+        public ActionResult Shoutbox()
+        {
+            return View();
+        }
+    }
+}
---
+++ LiveChat/LiveChatApp/Hubs/ShoutboxHub.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNet.SignalR;
+
+namespace LiveChatApp.Hubs
+{
+    public class ShoutboxHub : Hub
+    {
+        public void Send(string message)
+        {
+            Clients.All.addNewMessage(message);
+        }
+    }
+}
